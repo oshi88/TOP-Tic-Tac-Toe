f1d3d1509c3204dc4b8295f89d0f9d775f447aa0
oshi88 <guptad68@gmail.com>
2022-11-21 17:52:55 +0530

    added round

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Player = (sign) => {
     this.sign = sign;
     const signGet = () =>{
@@ -9,14 +11,43 @@ const Player = (sign) => {
 const gameBoard = (()=>{
     const gameBoardArr = ["","","","","","","","","",];
 
+    const setSign = (index,sign) =>{
+        if(index > gameBoardArr.length) return;
+        gameBoardArr[index] = 'X';
+    }
 
-})
+    const getSign = (index) =>{
+        return gameBoard[index];
+    }
+return {setSign,getSign,gameBoardArr};
+
+})();
 
 
-function inputFunction(e){
+const displayControl = (() => {
+    const divTile = document.querySelectorAll(".tile");
+    divTile.forEach((tile) =>
+    tile.addEventListener("click", (e) =>{
+        gameBoard.setSign(e.target.dataset.index);
+        divTile[e.target.dataset.index].innerText = gameControl.currentPlayerSign();
+    }))
+return {divTile}
+})();
+
+
+const gameControl = (() => {
+
     const playerX = Player("X");
     const playerO = Player("O");
-    const id = e.target.id;
-    console.log(id);
+    let round = 1;
+    const playRound = (tileIndex) =>{
 
-}
+    }
+    const currentPlayerSign = () =>{
+        var sign = round % 2 === 1? playerX.signGet() : playerO.signGet()
+         round++
+         return sign;
+    }
+    
+        return{currentPlayerSign,round}
+})();
